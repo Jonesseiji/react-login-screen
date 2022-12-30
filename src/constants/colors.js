c84a67b53bba002bfc78ad9fea662e6ef8c184da
Jonesseiji <jonesseiji9@gmail.com>
2022-12-30 08:15:52 -0300

    feat: add constant values

---
+++ src/constants/colors.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+    PRIMARY_ACCENT_COLOR: "rgb(255, 51, 102)",
+    RED_PRIMARY: "#d2002d",
+    GREEN_PRIMARY: "#006E41",
+    BLUE: "#0A3782",
+    DEFAULT_LIGHT: "#FFF",
+    DEFAULT_DARK: "#282828",
+    SECONDARY_LIGHT: "#eee",
+    GRAY_VARIANT_1: "#505050",
+    GRAY_VARIANT_2: "#d9d9d9",
+    GRAY_VARIANT_3: "#A3A3A3",
+    GRAY_VARIANT_4: "#ddd", // used for border
+  };
+  
+  export const GRADIENT_COLORS = {
+    BLACK:
+      "linear-gradient(45deg, #151616 10%, rgba(21,22,22,0.3) 50%, transparent 100%), linear-gradient(15deg, #151616 10%, rgba(21,22,22, 0.3) 50%, transparent 100%), linear-gradient(transparent 80%, #151616 100%)",
+  };
---
+++ src/constants/strings.js
@@ -0,0 +1,4 @@
+export const BUTTON_TEXT = {
+  DEFAULT: "Digite seu texto",
+  LOGIN: "Entrar",
+};
